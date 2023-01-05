cc580e06350cb491ae935e3183df3eafc89ec320
Vu Nguyen <vu.nguyen@techsoft.asia>
2023-01-05 21:33:34 +0700

    update proj

--- api/index.py
+++ api/index.py
@@ -1,6 +1,12 @@
-from flask import Flask
 
+from recommendation import MusicRecommendation
+from flask import Flask, redirect, url_for, request
+import joblib
 app = Flask(__name__)
+
+music_recommend = MusicRecommendation()
+file_name = 'model.joblib'
+song_cluster_pipeline = joblib.load(file_name)
 
 @app.route('/')
 def home():
