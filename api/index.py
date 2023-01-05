79abe4814cd63cc2405002a775a8034bbac10676
Vu Nguyen <vu.nguyen@techsoft.asia>
2023-01-05 21:34:19 +0700

    update

--- api/index.py
+++ api/index.py
@@ -8,10 +8,50 @@ music_recommend = MusicRecommendation()
 file_name = 'model.joblib'
 song_cluster_pipeline = joblib.load(file_name)
 
+
 @app.route('/')
 def home():
     return 'Hello, World!'
 
+
 @app.route('/about')
 def about():
     return 'About'
+
+
+@app.route('/recommend', methods=['GET'])
+def recommendation():
+    seedList = request.get_json()
+    result = music_recommend.main(seedList=seedList, song_cluster_pipeline=song_cluster_pipeline)
+
+    return result
+
+
+@app.route('/auto-gen/fetch-track', methods=['GET'])
+def fetch_track():
+    result = music_recommend.fetchTrack()
+    return result
+
+
+# @app.route('/track/lyric', methods=['GET'])
+# def getTrackLyric():
+#     track_name = request.get_data()
+#     result = music_recommend.getTrackLyrics(name=track_name)
+#     return result
+
+
+# data analyze
+@app.route('/auto-gen/artists', methods=['GET'])
+def get_artists():
+    artists = music_recommend.getArtists()
+    return artists
+
+
+@app.route('/auto-gen/getTrack', methods=['GET'])
+def get_tracks():
+    seedList = request.get_json()
+    music_list = []
+    for r in seedList:
+        track = music_recommend.getTrackInformation(trackId=r['id'], year=r['year'])
+        music_list.append(track)
+    return music_list
